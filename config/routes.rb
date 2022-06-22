01dfcdad2dad6954c366f01a9ac652e0aa12e37b
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-22 14:04:49 -0700

    inclusions in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
-  resources :purchases
-  resources :castles
-  resources :users
+  resources :purchases, only: %i[index show create]
+  resources :castles, only: %i[index show update]
+  resources :users, only: %i[create destroy]
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
