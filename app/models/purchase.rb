f9e0fb5fbcc81f3118cd4b4c65dada74f3ae70c7
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-22 13:58:21 -0700

    added validations to models

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,6 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :castle
+
+  validates :purchase, uniqueness: { scope: %i[user_id castle_id] }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    has_many :purchases
-    has_many :castles, through: :purchases
+  has_many :purchases
+  has_many :castles, through: :purchases
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 end
