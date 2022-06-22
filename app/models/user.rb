0a05649e6eba7f9d1aa5ec110bb88e60ebd28f8d
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-22 14:01:22 -0700

    added more val;idations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,21 @@ class User < ApplicationRecord
   has_many :purchases
   has_many :castles, through: :purchases
 
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: {
+              minimum: 4,
+              too_short: 'User name must be at least 4 characters.',
+              maximum: 12,
+              too_long: '12 characters is the maximum allowed.',
+            }
+  validates :password,
+            presence: true,
+            length: {
+              minimum: 4,
+              too_short: 'Password must be at least 4 characters.',
+              maximum: 10,
+              too_long: '10 characters is the maximum allowed.',
+            }
 end
