ceaffb819fe1bfd067fe4409d2a0a7da179d4c09
Kirsten <kirsten.staggs@gmail.com>
2022-06-23 12:25:05 -0600

    added migration file to change password to password_digest for login auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_035647) do
+ActiveRecord::Schema.define(version: 2022_06_23_182036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_035647) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
