0b471e61c9715ba094e17770935d82ee119d83f3
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-23 17:14:55 -0700

    Kori had too many castles.

--- client/src/components/Castle.js
+++ client/src/components/Castle.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 function Castle({ castle, user, buyCastle }) {
-  let [sold, setSold] = useState(false);
-
   const history = useHistory();
   function buyCastle() {
-    console.log("You are buying", castle.title);
+    // console.log("You are buying", castle.title);
     let data = { castle_id: castle.id, user_id: user.id };
     // ----------- FETCH AREA -------------- //
     fetch("/purchases", {
@@ -27,17 +25,13 @@ function Castle({ castle, user, buyCastle }) {
         console.error("Error adding new purchase:", error);
       });
 
-    setSold(true);
-
     // ------------ FETCH AREA ------------ //
   }
-  console.log("is sold? ", sold);
 
   return (
     <div className="render-castles">
       <h2>{castle.title}</h2>
       <h3>{castle.location}</h3>
-      <h3>IS SOld? {sold}</h3>
       <h4>${castle.price}</h4>
       <img src={castle.image} alt="castle" className="castle-imgs" />
       <p>{castle.description}</p>
